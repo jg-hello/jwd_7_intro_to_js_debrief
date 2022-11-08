9889537b36c0909436b607e28961daf480fd88b5
JG <jg.hello@proton.me>
2022-11-07 22:58:34 -0500

    I have made an edit to js file

--- js/app.js
+++ js/app.js
@@ -9,8 +9,26 @@
 // Reference Codecademy lessons: Introduction- Arithmetic Opertaors, Conditional Statements lessons 3, 4, 5,and 9
 
 //Write your code for GenBuzz below this line:
+const myNumber = 25;
+const num3 = 10;
 
+if (Number.isInteger(myNumber)){
+    console.log('Is Number');
+}else{
+    console.log('This is not a number');
+}
 
+if (myNumber % 3 == 0){ //checks if number is divisble by 3 evenly
+    console.log('gen!');
+}else{
+    console.log(myNumber);
+}
+
+if (myNumber % 5 == 0){ //checks if number is divisble by 5 evenly
+    console.log('Buzz!');
+}else{
+    console.log(myNumber);
+}
 
 
 
@@ -28,8 +46,38 @@
 // If the variable input is not an item in the store, then print 'Invalid Item' to the console.
 
 //Write your code for the E-Commerce item list below this line:
+/* let item = '';
+const shoes = 50;
+const jeans = 25;
+const hat = 12;
+const socks = 2; 
+switch (item) {
+    case 'shoes':
+        console.log(`Shoes are ${shoes}`);
+        break;
+} */
 
+const item = 'hat';
 
+switch (item) {
+    case 'shoes':
+        console.log('Shoes are $50');
+        break;
+    case 'jeans':
+        console.log('Jeans are $25');
+        break;
+    case 'jeans':
+        console.log('Jeans are $25');   
+        break; 
+    case 'hat':
+        console.log('Hats are $12');   
+        break; 
+    case 'socks':
+        console.log('Socks are $2');   
+        break; 
+    default:
+        console.log('Invalid Item');
+}
 
 
 //3. Print a random integer (whole number) between 50(inclusive) and 100(exclusive) to the console
@@ -37,5 +85,12 @@
 
 //Write your code below this line:
 
+const min = 50;
+const max = 100;
 
+//const called  a which finds a random whole number between const min/max(50 and 100)
+const a = Math.floor(Math.random() * (max - min + 1)) + min; 
+
+// shows that random number between 50 - 100
+console.log(`Random value between ${min} and ${max} is ${a}`);
 
